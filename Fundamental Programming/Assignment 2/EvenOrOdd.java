2f7b42b955c721d7d40e31e8568689d975db74c9
rohitLomga <rohitlomga505@gmail.com>
2025-01-23 09:55:06 +0530

    Update EvenOrOdd.java

--- Fundamental Programming/Assignment 2/EvenOrOdd.java
+++ Fundamental Programming/Assignment 2/EvenOrOdd.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class EvenOrOdd {
     public static void main(String[] args){
-        int number = 15;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number : ");
+        int number = scanner.nextInt();
 
         if((number % 2) == 0){
             System.out.println(number + " is an even integer");
