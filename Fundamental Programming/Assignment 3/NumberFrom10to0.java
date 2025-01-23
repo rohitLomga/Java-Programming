61af7b280761f0693f47bda0ba2e4be4c395c3e9
rohitLomga <rohitlomga505@gmail.com>
2025-01-23 16:41:52 +0530

    Update NumberFrom10to0.java

--- Fundamental Programming/Assignment 3/NumberFrom10to0.java
+++ Fundamental Programming/Assignment 3/NumberFrom10to0.java
@@ -3,7 +3,14 @@ package Assignment3;
 public class NumberFrom10to0 {
     public static void main(String [] args){
         int j = 10;
+        int i = 0;
 
+        System.out.println("This is 0 to 10");
+        while(i <= 0){
+            System.out.println(i);
+            i++;
+        }
+        
         // LOOP TO PRINT FROM 10 TO 0
         System.out.println("This is 10 to 0");
         while(j >= 0){
