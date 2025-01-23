160d8035239910f8bd7412b5276d091e89b9ea00
rohitLomga <rohitlomga505@gmail.com>
2025-01-23 09:50:05 +0530

    Update CheckTwoNumbers.java

--- Fundamental Programming/Assignment 2/CheckTwoNumbers.java
+++ Fundamental Programming/Assignment 2/CheckTwoNumbers.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+// 1 . Comparision of two numbers
+
 public class CheckTwoNumbers {
     public static void main(String[] args){
 
-        int num1 = 15;
-        int num2 = 15;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter num1 : ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter num2 : ");
+        int num2 = scanner.nextInt();
 
         if(num1 == num2){
             System.out.println("Number1 and Number2 are equal");
